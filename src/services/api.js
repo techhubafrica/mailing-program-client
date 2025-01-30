f20ecee9dcac600d575c602a8527f1e4f4e7a34d
ayodeji akinola <ayodejiakinola007@gmail.com>
2025-01-30 13:14:21 +0100

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-     //baseURL: "https://tech-hub-mailing-program-server.onrender.com/api"
-   baseURL: "http://localhost:5000/api",
+     baseURL: "https://tech-hub-mailing-program-server.onrender.com/api"
+   //baseURL: "http://localhost:5000/api",
 })
 export const fetchCampaigns = async ({ status = "", search = "" }) => {
   try {
